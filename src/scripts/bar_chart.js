4354bc69872ed2a06d73e2d64ed22d95109c371f
Paul Kim <paulwkim30@gmail.com>
2021-09-07 14:44:28 -0700

    add scaling functions

--- src/scripts/bar_chart.js
+++ src/scripts/bar_chart.js
@@ -9,12 +9,30 @@ class BarChart {
     this.height = height;
   };
 
+  scaleData(data) {
+    const x = d3.scaleLinear()
+      .domain([ 0, data[0].value ])
+      .range([ 0, this.width ]);
+
+    const y = d3.scaleBand()
+      .range([ 0, this.height ])
+      .domain(data.map( d => d.country))
+      .padding(0.1);
+
+    return [x, y];
+  };
+
   display(type) {
     fetch(`https://disease.sh/v3/covid-19/countries`)
       .then( apiResponse => apiResponse.json() )
       .then( data => {
-        const newData = formatBarData(data, type);
+        const newData = formatBarData(data, type); // format data
+
+        let [x, y] = this.scaleData(newData);
+
         console.log(newData);
+        console.log(x);
+        console.log(y);
       });
   };
 };
